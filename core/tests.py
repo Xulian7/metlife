7f9d4e549a1abddfdfd53f767aebbf61f5359256
Xulian7 <jfgrossop@outlook.com>
2026-09-03 10:10:46 -0500

    version 2

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class DashboardRenderTests(TestCase):
+    def test_dashboard_renders_with_static_banner(self):
+        user = get_user_model().objects.create_user(username="consultor")
+        self.client.force_login(user)
+        response = self.client.get(reverse("dashboard"), follow=True)
+        self.assertContains(response, "Panel 360")
+        self.assertContains(response, "dashboard-top.svg")
